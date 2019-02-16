41ca64ea4c7c3aba2ad12af252ef88d35654ef23
MartinHowarth <howarth.martin@gmail.com>
2019-02-16 17:17:29 +0000

    tweaks tests

--- end_product/tests/test_implemented_thing.py
+++ end_product/tests/test_implemented_thing.py
@@ -2,6 +2,7 @@ from end_product.implemented_thing import ImplementedThing
 
 
 def test_ImplementedThing():
+    # This test takes very little time to run
     thing = ImplementedThing()
     assert hasattr(thing, 'is_ready')
     assert hasattr(thing, 'is_healthy')
--- end_product/tests/test_implemented_thing_expensive.py
+++ end_product/tests/test_implemented_thing_expensive.py
@@ -2,6 +2,7 @@ from end_product.implemented_thing_expensive import ImplementedThingExpensive
 
 
 def test_ImplementedThingExpensive():
+    # This test takes >5 seconds to run, because importing ExpensivePlugin sleeps for 5.
     thing = ImplementedThingExpensive()
     assert hasattr(thing, 'is_ready')
     assert hasattr(thing, 'is_healthy')
